d88f9167cec9d269b722441e2a178fd7e4e2be4d
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2023-10-24 12:17:25 +0200

    flex

--- CSS-SASS/gulpfile.js
+++ CSS-SASS/gulpfile.js
@@ -1,16 +1,28 @@
-const gulp = require('gulp');
+const {src,watch, dest} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+var link = require('gulp-coffeelint');
 
 
-function compilaSCSS() {
-    return gulp.src('scss/**/*.scss')
+function compilaFlex() {
+    return src('flex/scss/**/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(dest('flex/css'));
+}
+function compilaSCSS() {
+    return src('scss/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(dest('dist/css'));
 }
 function defaultTask(cb) {
     // place code for your default task here
     cb();
 }
 
+
+function vigilaSCSS(){
+    watch('flex/scss/**/*.scss', compilaFlex);
+}
 exports.compilaEstils = compilaSCSS;
 exports.default = defaultTask;
+exports.compilaflex = compilaFlex;
+exports.vigila = vigilaSCSS;
